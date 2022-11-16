b6a96a3e874b0e9b9d515b8e7481c9e089efe19c
zapaz.eth <alain@kredeum.com>
2022-11-16 11:36:38 +0100

    fix & refacto v0.0.6

--- src/mapping.ts
+++ src/mapping.ts
@@ -2,7 +2,8 @@ import { BigInt, store } from '@graphprotocol/graph-ts';
 import { Transfer, ERC721 } from '../generated/ERC721/ERC721';
 import { Token } from '../generated/schema';
 
-let ZERO_ADDRESS_STRING = '0x0000000000000000000000000000000000000000';
+const ZERO_ADDRESS_STRING = '0x0000000000000000000000000000000000000000';
+const CHAIN_ID = 5;
 
 function setCharAt(str: string, index: i32, char: string): string {
   if (index > str.length - 1) return str;
@@ -22,47 +23,50 @@ function normalize(strValue: string): string {
   }
 }
 
-export function handleTransfer(event: Transfer): void {
-  const chainId = 5;
-  let tokenId = event.params.tokenId;
+function eventId(event: Transfer): string {
+  return `eip155:${CHAIN_ID}/erc721:${event.address.toHex()}/${event.params.tokenId.toString()}`;
+}
 
-  let id = `eip155:${chainId}/erc721:${event.address.toHex()}/${tokenId.toString()}`;
+function mintEvent(event: Transfer): void {
+  let eip721Token = new Token(eventId(event));
 
-  let from = event.params.from.toHex();
-  let to = event.params.to.toHex();
-  let contract = ERC721.bind(event.address);
+  let metadataURItried = ERC721.bind(event.address).try_tokenURI(event.params.tokenId);
+  eip721Token.tokenURI = metadataURItried.reverted ? "" : normalize(metadataURItried.value);
 
-  if ((from == ZERO_ADDRESS_STRING) && (to == ZERO_ADDRESS_STRING)) return;
+  eip721Token.save();
+}
 
-  if (from == ZERO_ADDRESS_STRING) {  // MINT 
-    let eip721Token = new Token(id);
+function transferEvent(event: Transfer): void {
+  let eip721Token = Token.load(eventId(event));
 
-    let metadataURItried = contract.try_tokenURI(tokenId);
+  if (eip721Token) {
+    let metadataURIprevious = eip721Token.tokenURI;
+
+    let metadataURItried = ERC721.bind(event.address).try_tokenURI(event.params.tokenId);
     if (!metadataURItried.reverted) {
-      eip721Token.tokenURI = normalize(metadataURItried.value);
-    }
+      let metadataURInew = normalize(metadataURItried.value);
 
-    eip721Token.save();
-  } else {
-    if (to == ZERO_ADDRESS_STRING) {  // BURN
-      store.remove('Token', id);
-    } else {                          // TRANSFER
-      let eip721Token = Token.load(id);
-
-      if (eip721Token) {
-        let metadataURIprevious = eip721Token.tokenURI;
-        let metadataURItried = contract.try_tokenURI(tokenId);
-
-        if (!metadataURItried.reverted) {
-          let metadataURInew = normalize(metadataURItried.value);
-
-          if (metadataURInew != metadataURIprevious) {
-            eip721Token.tokenURI = metadataURInew;
-            eip721Token.save();
-          }
-        }
+      if (metadataURInew != metadataURIprevious) {
+        eip721Token.tokenURI = metadataURInew;
+        eip721Token.save();
       }
     }
   }
+}
 
+function burnEvent(event: Transfer): void {
+  store.remove('Token', eventId(event));
+}
+
+export function handleTransfer(event: Transfer): void {
+  let from = event.params.from.toHex();
+  let to = event.params.to.toHex();
+
+  if (from == ZERO_ADDRESS_STRING) {
+    if (to == ZERO_ADDRESS_STRING) return;
+    else mintEvent(event);
+  } else {
+    if (to == ZERO_ADDRESS_STRING) burnEvent(event);
+    else transferEvent(event);
+  }
 }
